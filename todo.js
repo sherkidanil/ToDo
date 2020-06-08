38c06dc2240fe92db092387fb6319c20dd9c104d
sherkidanil <66497330+sherkidanil@users.noreply.github.com>
2020-06-09 01:29:07 +0300

    Add files via upload

--- todo.js
+++ todo.js
@@ -1,11 +1,28 @@
 addTask.onclick = function() { 
     let task=document.createElement('div');
     task.className='worksheet';
-    task.innerHTML=""
     let taskName=document.getElementsByTagName('input')[0].value;
     let priority=document.getElementById('select').value;
-
-
+    task.innerHTML=`<div class="priorityTask" id="newPriorityTask"></div>
+    <div class="task">
+        <div class="taskName" id="newTaskName"></div>
+        <div class="taskCancel"><button>
+            <img src="img/close.png">
+        </button></div>
+        <div class="taskDate"><p>Тут будет дата</p></div>
+        <div class="taskDone"><button>
+            <img src="img/check.png">
+        </button></div>
+    </div>
+    <div class="button" name="deleteTask"><button>
+        <img src="img/delete.png">
+    </button></div>
+    </div>`
+    let newTaskName=document.getElementById('newTaskName');
+    let newPriority=document.getElementById('newPriorityTask');
+    newTaskName=taskName;
+    newPriority=priority;
+    document.body.append(task);
 }
 
 /* часть кода, которая отвечает за изменение названия задачи */
