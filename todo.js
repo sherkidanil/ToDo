c63ffff698398c6a774b808b17cfcf691f2b21e4
sherkidanil <66497330+sherkidanil@users.noreply.github.com>
2020-06-21 17:24:58 +0300

    Add files via upload

--- todo.js
+++ todo.js
@@ -1,8 +1,10 @@
 let n=0;
-addTask.onclick = function() {
 
+addTask.onclick = function() {
   taskName=document.getElementsByTagName('input')[0].value;
   priority=document.getElementById('select').value;
+  let date=()=>new Date();
+  dateFromAdd=date();
   let ws=new Worksheet(n, taskName, priority);
   ws.createTask(ws.taskName, ws.priority);
   ws.taskName=document.getElementsByName('caption')[n];
@@ -11,11 +13,58 @@ addTask.onclick = function() {
   ws.deleteTask(deleteButton);
   doneButton=document.getElementsByName("doneBut")[n];
   cancelButton=document.getElementsByName("cancelBut")[n];
-  ws.done(doneButton,cancelButton);
+  done(doneButton,cancelButton);
+  cancel(doneButton,cancelButton);
+  taskDate=document.getElementsByName('taskDate')[n];
+  ws.date(taskDate, dateFromAdd);
   n++;
+};
+
+done=function(doneButton, cancelButton) {
+  doneButton.onclick=function() {
+    let doneDate=()=>new Date();
+    doneInf=document.createElement('p');
+    doneButton.replaceWith(doneInf);
+    doneInf.innerHTML='Завершено в <time>'+doneDate()+'</time>';
+    cancelButton.remove();
+  };
+};
+cancel=function(doneButton, cancelButton) {
+  cancelButton.onclick=function() {
+    let cancelDate=()=>new Date();
+    cancelInf=document.createElement('p');
+    doneButton.replaceWith(cancelInf);
+    сancelInf.innerHTML='Отменено в '+cancelDate();
+    cancelButton.remove();
+  };
+};
+
+
+highInput.onclick = function() {
+  let array=document.querySelectorAll('i');
+  for (let i=0; i<n; ++i) {
+    let mean=array[i].innerHTML;
+    if (mean !== 'Выcокий') {array[i].parentElement.parentElement.hidden=!array[i].parentElement.parentElement.hidden};
+  }
 }
 
-function Worksheet(num, taskName, priority) {
+mediumInput.onclick = function() {
+  let array=document.querySelectorAll('i');
+  for (let i=0; i<n; ++i) {
+    let mean=array[i].innerHTML;
+    if (mean !== 'Средний') {array[i].parentElement.parentElement.hidden=!array[i].parentElement.parentElement.hidden};
+  }
+}
+
+lowInput.onclick = function() {
+  let array=document.querySelectorAll('i');
+  for (let i=0; i<n; ++i) {
+    let mean=array[i].innerHTML;
+    if (mean !== 'Низкий') {array[i].parentElement.parentElement.hidden=!array[i].parentElement.parentElement.hidden};
+  }
+}
+
+function Worksheet(num, taskName, priority, taskDate) {
   this.num=num;
   this.taskName=taskName;
   this.priority=priority;
@@ -23,7 +72,7 @@ function Worksheet(num, taskName, priority) {
     let task=document.createElement('div');
     task.className='worksheet';
     task.innerHTML=`
-      <div class="priorityTask" id="priorityTask">Высокий</div>
+      <div class="priorityTask"><i><p id="priorityTask">Высокий</p></i></div>
       <div class="task" id="task">
         <div class="taskName" id="taskName" name="caption"><div id="newTaskName">Не задано</div></div>
         <div class="taskCancel"><button name='cancelBut'>
@@ -32,7 +81,7 @@ function Worksheet(num, taskName, priority) {
         <div class="taskDone"><button name="doneBut">
             <img src="img/check.png">
         </button></div>
-        <div class="taskDate">Тут будет дата</div>
+        <div class="taskDate" id="taskDate"><p name="taskDate"> Тут будет дата</p></div>
     </div>
     <div class="button"><button id="deleteBut" name="deleteBut">
         <img src="img/delete.png">
@@ -43,9 +92,15 @@ function Worksheet(num, taskName, priority) {
     newTaskName.replaceWith(taskName);
     newTaskName.className="taskName";
     let priorityTask=document.getElementById('priorityTask');
-    priorityTask.replaceWith(priority);} 
+    priorityTask.replaceWith(priority);
+  } 
 
-    this.changeName=function(taskCaption) {
+  this.date=function(taskDate, dateFromAdd){
+      let newDate=dateFromAdd;
+      taskDate.innerHTML='<time>'+newDate+'</time>';
+  }
+    
+  this.changeName=function(taskCaption) {
       taskCaption.onclick = function() {
         editStart();
         };
@@ -73,6 +128,7 @@ function Worksheet(num, taskName, priority) {
         taskCaption.innerHTML = area.value;
         area.replaceWith(taskCaption);
       };
+  };
 
     this.deleteTask=function(deleteButton) {
         deleteButton.onclick=function() {
@@ -82,15 +138,6 @@ function Worksheet(num, taskName, priority) {
       }
 
     this.isDone = false;
-    this.done=function(doneButton, cancelButton) {
-      doneButton.onclick=function() {
-        this.isDone=true;
-        doneInf=document.createElement('p');
-        doneInf.innerHTML='Завершено в ';
-        cancelButton.remove();
-        doneButton.replaceWith(doneInf);
-      };
-    };
-    };
+    this.isCancel= false;
+  };
 
-}
